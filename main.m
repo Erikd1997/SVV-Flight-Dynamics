c94f292779c347f366d593ead766a8251350a2e0
Erikd1997 <erik.dedding@hotmail.nl>
2018-03-08 12:34:40 +0100

    Added commands to plot system responses

--- main.m
+++ main.m
@@ -1,3 +1,7 @@
+clc
+clear all
+close all
+
 %Call the file containing all variables
 Cit_par
 
@@ -11,5 +15,13 @@ c = load('Cit_par.mat');
 %Create initial state vector
 x0 = [c.V0, c.alpha0, c.th0, 0];
 
-t = 0:1:150;
-u = ones(1,length(t))*-0.005;
+tS = 0:1:150;
+uS = ones(1,length(tS))*-0.005;
+
+tA = 0:1:150;
+uA = [ones(1,length(tA))*-0.005; ones(1,length(tA))*-0.005];
+
+figure(1)
+lsim(sysS,uS,tS,x0)
+figure(2)
+lsim(sysA,uA,tA,x0)
