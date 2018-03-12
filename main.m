49dcbe7bd33ef64c10b9d0db81861f52b8af84ae
Erikd1997 <erik.dedding@hotmail.nl>
2018-03-12 16:14:20 +0100

    Small updates and error fixes

--- main.m
+++ main.m
@@ -1,27 +1,33 @@
-clc
-clear all
-close all
+%clc
+%close all
 
 %Call the file containing all variables
 Cit_par
 
 %Save these values in a struct
 save('Cit_par.mat')
+
+%Delete all saved variables from workspace
+clear
+
+%Load in values in a struct 'c'
 c = load('Cit_par.mat');
 
 %Create state-space-system
 [sysS, sysA] = state_space_system(c);
 
 %Create initial state vector
-x0 = [c.V0, c.alpha0, c.th0, 0];
+x0S = [c.V0,  c.alpha0, c.th0, 0];
+x0A = [  0,       0,      0,   0];
 
-tS = 0:1:150;
-uS = ones(1,length(tS))*-0.005;
+tS = 0:0.1:500;
+uS = deg2rad(sin(tS));                    %de = 15 degrees
 
-tA = 0:1:150;
-uA = [ones(1,length(tA))*-0.005; ones(1,length(tA))*-0.005];
+tA = 0:0.1:500;
+uA = [deg2rad(sin(tA)); deg2rad(sin(tA))]; %da = 15degrees, dr = 26 degrees
+%uA(:,1:20) = ones(2,20)*deg2rad(1);
 
 figure(1)
-lsim(sysS,uS,tS,x0)
+lsim(sysS,uS,tS,x0S)
 figure(2)
-lsim(sysA,uA,tA,x0)
+lsim(sysA,uA,tA,x0A)
--- state_space_system.m
+++ state_space_system.m
@@ -7,7 +7,7 @@ function [sysS, sysA] = state_space_system(c)
 C1_S = [-2*c.muc*c.c/c.V0^2,             0,               0,               0             ;
                0,           (c.CZadot-2*c.muc)*c.c/c.V0,  0,               0             ;
                0,                        0,               1,               0             ;
-               0                 c.Cmadot*c.c/c.V0,       0,  -2*c.muc*c.KY2^2*(c.c/c.V0)^2];
+               0                 c.Cmadot*c.c/c.V0,       0,  -2*c.muc*c.KY2*(c.c/c.V0)^2];
            
 C2_S = [c.CXu/c.V0, c.CXa,  c.CZ0,      c.CXq*c.c/c.V0     ;
         c.CZu/c.V0, c.CZa, -c.CX0, c.c/c.V0*(c.CZq+2*c.muc);
@@ -22,10 +22,7 @@ C3_S = [c.CXde;
 % Create A, B, C and D matrices
 A_S = -C1_S\C2_S;
 B_S = -C1_S\C3_S;
-C_S = [ 1, 0, 0, 0;
-        0, 1, 0, 0;
-        0, 0, 1, 0;
-        0, 0, 0, 1];
+C_S = eye(4);
 D_S = 0;
     
 %Create state space system
@@ -34,34 +31,28 @@ sysS.OutputName = {'u', '\alpha', '\theta', 'q'};
 
 %% Asymmetric model
 % Defining C1, C2 and C3
-C1_A = [(c.CYbdot-2*c.mub)*c.b/c.V0, 0,                    0,                   0             ;
-               0,                    1,                    0,                   0             ;
-               0,                    0,   -2*c.mub*c.KX2^2*(c.b/c.V0)^2,  2*c.mub*c.KXZ*(c.b/c.V0)^2;
-        c.Cnbdot*c.b/c.V0,           0,     2*c.mub*c.KXZ*(c.b/c.V0)^2,  -2*c.mub*c.KZ2^2*(c.b/c.V0)^2];
-           
+C1_A = [(c.CYbdot-2*c.mub)*c.b/c.V0, 0,                  0,                          0             ;
+                    0,               1,                  0,                          0             ;
+                    0,               0,   -2*c.mub*c.KX2*(c.b/c.V0)^2,   2*c.mub*c.KXZ*(c.b/c.V0)^2;
+           c.Cnbdot*c.b/c.V0,        0,    2*c.mub*c.KXZ*(c.b/c.V0)^2,  -2*c.mub*c.KZ2*(c.b/c.V0)^2];
+            
 C2_A = [c.CYb, c.CL, c.CYp*c.b/(2*c.V0), c.b/(2*c.V0)*(c.CYr-4*c.mub);
-          0,     0,        -1,                  0                  ;
-        c.Clb,   0,  c.Clp*c.b/(2*c.V0), c.Clr*c.b/(2*c.V0)        ;
-        c.Cnb,   0,  c.Cnp*c.b/(2*c.V0), c.Cnr*c.b/(2*c.V0)        ];
+          0,     0,        -1,                       0               ;
+        c.Clb,   0,  c.Clp*c.b/(2*c.V0),      c.Clr*c.b/(2*c.V0)     ;
+        c.Cnb,   0,  c.Cnp*c.b/(2*c.V0),      c.Cnr*c.b/(2*c.V0)     ];
 
 C3_A = [c.CYda, c.CYdr;
-          0,   0  ;
+          0,       0  ;
         c.Clda, c.Cldr;
         c.Cnda, c.Cndr];
 
 % Create A, B, C and D matrices
 A_A = -C1_A\C2_A;
 B_A = -C1_A\C3_A;
-C_A = [ 1, 0, 0, 0;
-        0, 1, 0, 0;
-        0, 0, 1, 0;
-        0, 0, 0, 1];
-D_A = [ 0, 0 ;
-        0, 0 ;
-        0, 0 ;
-        0, 0 ];
+C_A =    eye(4);
+D_A =      0;
 
 %Create state space system
 sysA = ss(A_A, B_A, C_A, D_A);
-sysA.OutputName = {'beta', '\phi', 'p', 'r'};
+sysA.OutputName = {'\beta', '\phi', 'p', 'r'};
 end
