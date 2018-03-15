b86de7a8081dc2bd707b8a063ac7d5dd2672836c
joriskoopman <joris.p.koopman@gmail.com>
2018-03-15 17:12:52 +0100

    fixed the order of the columns

--- ThrustFile.m
+++ ThrustFile.m
@@ -1,19 +1,19 @@
-function T = ThrustFile(c,hp_col, M_col, Mf1_col, Mf2_col, T_temp_col)
+function T = ThrustFile(c,hp_col, M_col, T_temp_col, Mf1_col, Mf2_col)
 %This function obtains the thrust per engine for each moment in time
 %% Firstly, the required parameters for the thrust.exe file are calculated
 T_temp_ISA = c.Temp0 + c.lambda*hp_col;
 
 Delta_T_temp_col = T_temp_col - T_temp_ISA;
 
-% fid = fopen('matlab.dat','w');
-% textarray = string(num2str(hp_col,4))+' '+string(num2str(M_col,4))+' '+string(num2str(Mf1_col,4))+' '+string(num2str(Mf2_col,4))+' '+string(num2str(Delta_T_temp_col,4));
-% text = textarray(1);
-% for i = 2:length(textarray)
-%     text = text + '\r\n' + textarray(i);
-% end
-% %The matlab.dat file is written and saved
-% fprintf(fid,text);
-% fclose(fid);
+fid = fopen('matlab.dat','w');
+textarray = string(num2str(hp_col,4))+' '+string(num2str(M_col,4))+' '+string(num2str(Delta_T_temp_col,4))+' '+string(num2str(Mf1_col,4))+' '+string(num2str(Mf2_col,4));
+text = textarray(1);
+for i = 2:length(textarray)
+    text = text + '\r\n' + textarray(i);
+end
+%The matlab.dat file is written and saved
+fprintf(fid,text);
+fclose(fid);
 
 %% Run the thrust calculations
 !Thrust.exe
