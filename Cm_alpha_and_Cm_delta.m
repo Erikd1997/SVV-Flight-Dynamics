7003e584e95f224bba05360a658655dd3b9d0f8a
joriskoopman <joris.p.koopman@gmail.com>
2018-03-15 13:32:34 +0100

    incorporated the ISA_converter file

---
+++ Cm_alpha_and_Cm_delta.m
@@ -0,0 +1,42 @@
+%This program handles the calculation of Cm_alpha and Cm_delta from our
+%flight test data.
+
+%First, import the data from the excel file and convert them into SI-units:
+datasheet = 'Post_Flight_Datasheet_Flight_2_DD_6_3_2018_for_test.xlsx';
+
+%Load relevant sub-programs
+Cit_par
+ISA_converted
+
+ET          = xlsread(datasheet, 'C59:C65');
+hp          = xlsread(datasheet, 'D59:D65').*unitsratio('meter', 'feet');
+IAS         = convvel(xlsread(datasheet, 'E59:E65'), 'kts', 'm/s');
+alpha       = convang(xlsread(datasheet, 'F59:F65'), 'deg', 'rad');
+delta_e     = convang(xlsread(datasheet, 'G59:G65'), 'deg', 'rad');
+delta_e_tr  = convang(xlsread(datasheet, 'H59:H65'), 'deg', 'rad');
+F_e         = xlsread(datasheet, 'I59:I65');
+FF_l        = convvel((convmass(xlsread(datasheet, 'J59:J65'), 'lbm', 'kg')), 'km/h', 'km/s');
+FF_r        = convvel((convmass(xlsread(datasheet, 'K59:K65'), 'lbm', 'kg')), 'km/h', 'km/s');
+F_used      = convmass(xlsread(datasheet, 'L59:L65'), 'lbm', 'kg');
+TAT         = convtemp(xlsread(datasheet, 'M59:M65'), 'C', 'K');
+
+%Start with computing Cm_d:
+IAS_kts_Cm_d  = mean(xlsread(datasheet, 'E75:E76'));
+hp_ft_Cm_d   = mean(xlsread(datasheet, 'D75:D76'));
+TAT_C_Cm_d  = mean(xlsread(datasheet, 'M75:M76'));
+Delta_de_Cm_d  = convang((xlsread(datasheet, 'G75')-xlsread(datasheet, 'G76')), 'deg', 'rad');
+
+
+[h_Cm_d, V_Cm_d, T_Cm_d, p_Cm_d, rho_Cm_d] = ISA_converted(hp_ft_Cm_d, ...
+    IAS_kts_Cm_d, TAT_C_Cm_d)
+
+C_N      = W/(0.5*rho_Cm_d*V_c_Cm_d^2*S);
+
+%Delta_cg = cg[13]-cg[14]
+Delta_cg = -0.1
+
+Cm_d = -(1/Delta_de_Cm_d)*C_N*Delta_cg/c
+
+%Now compute Cm_alpha:
+
+
