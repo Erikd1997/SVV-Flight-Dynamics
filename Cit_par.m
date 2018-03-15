2b02bb051d40de71d4709e36b8535267c405f0f6
joriskoopman <joris.p.koopman@gmail.com>
2018-03-15 12:23:19 +0100

    added Cm_d

--- Cit_par.m
+++ Cit_par.m
@@ -43,6 +43,7 @@ lambda = -0.0065;         % temperature gradient in ISA [K/m]
 Temp0  = 288.15;          % temperature at sea level in ISA [K]
 R      = 287.05;          % specific gas constant [m^2/sec^2K]
 g      = 9.81;            % [m/sec^2] (gravity constant)
+p0     = 101325;          % [Pa]
 
 rho    = rho0*((1+(lambda*hp0/Temp0)))^(-((g/(lambda*R))+1));   % [kg/m^3]  (air density)
 W      = m*g;				                        % [N]       (aircraft weight)
