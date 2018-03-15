1e9d5e95f236fa2ed5356d93961aa7d7fcf6266d
joriskoopman <joris.p.koopman@gmail.com>
2018-03-15 15:22:05 +0100

    Updated sign convention
    I will, from now on, use the following sign convention:
    1. first letter/word represents symbol
    2. the next letter/word is the first subscript, if either a word is used to represent a symbol or both letters are capitals, an underscore is placed in between the two.
    3. for sub-subscripts an underscore seperates the two subscripts.
    4. if the sub-subscript is a capital letter, sub-sub-subscipts are appended in lower case without underscore, else an extra underscore is added.
    5. 's1' is appended to parameters if they contain an array of values from stationary measurement series 1, 's2' for the elevator trim measurements and 's3' if it contains measurements from the shifting centre of gravity measurements.

--- Cm_alpha_and_Cm_delta.m
+++ Cm_alpha_and_Cm_delta.m
@@ -8,34 +8,33 @@ datasheet = 'Post_Flight_Datasheet_Flight_2_DD_6_3_2018_for_test.xlsx';
 Cit_par
 ISA_converted
 
-ET          = xlsread(datasheet, 'C59:C65');
-hp          = xlsread(datasheet, 'D59:D65').*unitsratio('meter', 'feet');
-IAS         = convvel(xlsread(datasheet, 'E59:E65'), 'kts', 'm/s');
-alpha       = convang(xlsread(datasheet, 'F59:F65'), 'deg', 'rad');
-delta_e     = convang(xlsread(datasheet, 'G59:G65'), 'deg', 'rad');
-delta_e_tr  = convang(xlsread(datasheet, 'H59:H65'), 'deg', 'rad');
-F_e         = xlsread(datasheet, 'I59:I65');
-FF_l        = convvel((convmass(xlsread(datasheet, 'J59:J65'), 'lbm', 'kg')), 'km/h', 'km/s');
-FF_r        = convvel((convmass(xlsread(datasheet, 'K59:K65'), 'lbm', 'kg')), 'km/h', 'km/s');
-F_used      = convmass(xlsread(datasheet, 'L59:L65'), 'lbm', 'kg');
-TAT         = convtemp(xlsread(datasheet, 'M59:M65'), 'C', 'K');
+ET_s1          = xlsread(datasheet, 'C59:C65');
+hp_s1          = xlsread(datasheet, 'D59:D65').*unitsratio('meter', 'feet');
+IAS_s1         = convvel(xlsread(datasheet, 'E59:E65'), 'kts', 'm/s');
+alpha_s1       = convang(xlsread(datasheet, 'F59:F65'), 'deg', 'rad');
+delta_e_s1     = convang(xlsread(datasheet, 'G59:G65'), 'deg', 'rad');
+delta_e_tr_s1  = convang(xlsread(datasheet, 'H59:H65'), 'deg', 'rad');
+Fe_s1          = xlsread(datasheet, 'I59:I65');
+FFl_s1         = convvel((convmass(xlsread(datasheet, 'J59:J65'), 'lbm', 'kg')), 'km/h', 'km/s');
+FFr_s1         = convvel((convmass(xlsread(datasheet, 'K59:K65'), 'lbm', 'kg')), 'km/h', 'km/s');
+F_used_s1      = convmass(xlsread(datasheet, 'L59:L65'), 'lbm', 'kg');
+TAT_s1         = convtemp(xlsread(datasheet, 'M59:M65'), 'C', 'K');
 
 %Start with computing Cm_d:
-IAS_kts_Cm_d  = mean(xlsread(datasheet, 'E75:E76'));
-hp_ft_Cm_d   = mean(xlsread(datasheet, 'D75:D76'));
-TAT_C_Cm_d  = mean(xlsread(datasheet, 'M75:M76'));
-Delta_de_Cm_d  = convang((xlsread(datasheet, 'G75')-xlsread(datasheet, 'G76')), 'deg', 'rad');
+IAS_kts_s3   = mean(xlsread(datasheet, 'E75:E76'));
+hp_ft_s3     = mean(xlsread(datasheet, 'D75:D76'));
+TAT_C_s3     = mean(xlsread(datasheet, 'M75:M76'));
+Delta_de_s3  = convang((xlsread(datasheet, 'G75')-xlsread(datasheet, 'G76')), 'deg', 'rad');
 
-
-[h_Cm_d, V_Cm_d, T_Cm_d, p_Cm_d, rho_Cm_d] = ISA_converted(hp_ft_Cm_d, ...
-    IAS_kts_Cm_d, TAT_C_Cm_d)
+[h_Cm_d, V_Cm_d, T_Cm_d, p_Cm_d, rho_Cm_d] = ISA_converted(hp_ft_s3, ...
+    IAS_kts_s3, TAT_C_s3);
 
 C_N      = W/(0.5*rho_Cm_d*V_c_Cm_d^2*S);
 
 %Delta_cg = cg[13]-cg[14]
-Delta_cg = -0.1
+Delta_cg = -0.1;
 
-Cm_d = -(1/Delta_de_Cm_d)*C_N*Delta_cg/c
+Cm_d = -(1/Delta_de_s3)*C_N*Delta_cg/c
 
 %Now compute Cm_alpha:
 
